c2a53041fe0354686b8be91813029b256c89a590
DavidRamirez04 <ex0tic4838198@gmail.com>
2022-03-07 09:49:07 -0800

    WEBSITE

--- app/routes/default.py
+++ app/routes/default.py
@@ -6,6 +6,6 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def test():
-    return render_template('test.html')
+@app.route('/aboutme')
+def aboutme():
+    return render_template('aboutme.html')
